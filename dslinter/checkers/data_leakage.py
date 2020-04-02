54082d8595caaf04986c698bf1d281562097e917
MarkHaakman <m.p.a.haakman@student.tudelft.nl>
2020-04-02 13:48:40 +0200

    Add preprocessing classes in DataLeakageChecker

--- dslinter/checkers/data_leakage.py
+++ dslinter/checkers/data_leakage.py
@@ -35,75 +35,106 @@ class DataLeakageChecker(BaseChecker):
         "transform",
     ]
 
+    PREPROCESSING_CLASSES = [
+        "FunctionTransformer",
+        "KBinsDiscretizer",
+        "KernelCenterer",
+        "LabelBinarizer",
+        "LabelEncoder",
+        "MultiLabelBinarizer",
+        "MaxAbsScaler",
+        "MinMaxScaler",
+        "OneHotEncoder",
+        "OrdinalEncoder",
+        "PolynomialFeatures",
+        "PowerTransformer",
+        "QuantileTransformer",
+        "RobustScaler",
+        "StandardScaler",
+    ]
+
     def visit_call(self, node: astroid.Call):
         """
         When a Call node is visited, check whether it violated the rules in this checker.
 
         :param node: The node which is visited.
         """
-        # If the learning function is called on a learning class, rule is violated.
+        # If the learning function is called on an estimator, rule is violated.
         if (
             node.func is not None
             and hasattr(node.func, "attrname")
             and node.func.attrname in self.LEARNING_FUNCTIONS
-            and self._expr_is_learning_class(node.func.expr)
+            and self._expr_is_estimator(node.func.expr)
         ):
             self.add_message("sk-pipeline", node=node)
 
-    def _expr_is_learning_class(self, expr: astroid.node_classes.NodeNG) -> bool:
+    def _expr_is_estimator(self, expr: astroid.node_classes.NodeNG) -> bool:
         """
-        Evaluate whether the expression is a learning class.
+        Evaluate whether the expression is an estimator.
 
         :param expr: Expression to evaluate.
-        :return: True when the expression is a learning class.
+        :return: True when the expression is an estimator.
         """
-        if isinstance(expr, astroid.Call) and self._call_initiates_learning_class(expr):
+        if isinstance(expr, astroid.Call) and self._call_initiates_estimator(expr):
             return True
 
         # If expr is a Name, check whether the assignment can be found
-        # and the name is assigned a learning class.
+        # and the name is assigned an estimator.
         if isinstance(expr, astroid.Name):
             body_block = ASTUtil.search_body(expr)
             for child in body_block:
                 if (
                     isinstance(child, astroid.Assign)
                     or isinstance(child, astroid.AnnAssign)
-                    and self._learning_class_assigned(child)
+                    and self._estimator_assigned(child)
                 ):
                     return True
         return False
 
     @staticmethod
-    def _call_initiates_learning_class(call: astroid.Call) -> bool:
+    def _call_initiates_estimator(call: astroid.Call) -> bool:
         """
-        Evaluate whether a Call node is initiating a learning class.
+        Evaluate whether a Call node is initiating an estimator.
 
         :param call: Call to evaluate.
-        :return: True when a learning class is initiated.
+        :return: True when an estimator is initiated.
         """
         return (
             call.func is not None
             and hasattr(call.func, "name")
-            and call.func.name in Resources.get_hyperparameters().keys()
+            and call.func.name in DataLeakageChecker.get_estimator_classes()
         )
 
-    def _learning_class_assigned(self, assign: Union[astroid.Assign, astroid.AnnAssign]) -> bool:
+    @staticmethod
+    def get_estimator_classes() -> List[str]:
         """
-        Evaluate whether a learning class is assigned.
+        Get all estimator classes.
+
+        The list contains all learning classes and preprocessing classes which do something in the
+        fit function from sklearn.
+
+        :return: List of estimator classes.
+        """
+        learning_classes = list(Resources.get_hyperparameters().keys())
+        return learning_classes + DataLeakageChecker.PREPROCESSING_CLASSES
+
+    def _estimator_assigned(self, assign: Union[astroid.Assign, astroid.AnnAssign]) -> bool:
+        """
+        Evaluate whether an estimator is assigned.
 
         :param assign: Assign to evaluate.
-        :return: True when a learning class is assigned.
+        :return: True when an estimator is assigned.
         """
         if isinstance(assign, astroid.Assign):
             for target in assign.targets:
-                if isinstance(target.value, astroid.Call) and self._call_initiates_learning_class(
+                if isinstance(target.value, astroid.Call) and self._call_initiates_estimator(
                     target.value
                 ):
                     return True
         if (
             isinstance(assign, astroid.AnnAssign)
             and isinstance(assign.target.value, astroid.Call)
-            and self._call_initiates_learning_class(assign.target.value)
+            and self._call_initiates_estimator(assign.target.value)
         ):
             return True
         return False
