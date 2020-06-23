81b40914680c2a2108715c8fba543b4449366912
MarkHaakman <MarkHaakman@users.noreply.github.com>
2020-06-23 16:55:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
 """Setup script."""
 from setuptools import find_packages, setup
 
+# Get the long description from the README file
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="dslinter",
-    version="0.0.1",
-    description="A useful module",
-    author="Man Foo",
-    author_email="foomail@foo.com",
+    version="1.0.0",
+    description="Pylint plugin for linting data science and machine learning code, focussed on the libraries pandas and scikit-learn.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MarkHaakman/dslinter",
+    author="Mark Haakman",
     packages=find_packages(),
     package_data={"": ["*.pickle"]},
-    install_requires=["pylint>=2.0", "astroid", "mypy", "data-science-types", "pyspark-stubs"],
+    python_requires="~=3.5",
+    install_requires=["pylint~=2.0", "astroid~=2.4", "mypy", "data-science-types", "pyspark-stubs"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
 )
