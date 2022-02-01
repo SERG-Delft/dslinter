c7bdc7c5ba22badabfc165b4a32503bfb9a16512
Hynn01 <hynn0633@163.com>
2022-02-01 09:37:07 +0100

    add a test for UnnecessaryIterationTensorflowChecker

--- dslinter/test/checkers/test_unnecessary_iteration_tensorflow.py
+++ dslinter/test/checkers/test_unnecessary_iteration_tensorflow.py
@@ -13,3 +13,9 @@ class TestUnnecessaryIterationTensorflowChecker(pylint.testutils.CheckerTestCase
         with self.assertAddsMessages(pylint.testutils.Message(msg_id="iteration-tensorflow", node = for_node),):
             self.checker.visit_for(for_node)
 
+    def test_no_iteration(self):
+        """Test whether there is no message added when there is no iteration."""
+        module_tree = astroid.parse("import tensorflow as tf\nx = tf.random.uniform([500, 10])\nz = tf.reduce_sum(x, axis=0)")
+        for_node = module_tree.body[-1]
+        with self.assertNoMessages():
+            self.checker.visit_for(for_node)
