0203aae280a6fd0b62e96b8eeafd84602714421f
Hynn01 <hynn0633@163.com>
2022-02-11 10:45:54 +0100

    refactor MemoryReleaseTensorflowChecker

--- dslinter/checkers/memory_release_tensorflow.py
+++ dslinter/checkers/memory_release_tensorflow.py
@@ -1,6 +1,9 @@
 """Check whether the memory is freed in time."""
 import astroid
 from pylint.checkers import BaseChecker
+from dslinter.util.exception_handler import ExceptionHandler
+from dslinter.util.type_inference import TypeInference
+
 
 class MemoryReleaseTensorflowChecker(BaseChecker):
     """Check whether the memory is freed in time."""
@@ -8,6 +11,7 @@ class MemoryReleaseTensorflowChecker(BaseChecker):
     name = "memory_release_tensorflow"
     priority = -1
     msgs = {
+        #TODO: add the message code
         "":{
             "The memory has not freed in time.",
             "memory-release-tensorflow",
@@ -21,13 +25,52 @@ class MemoryReleaseTensorflowChecker(BaseChecker):
         "Model"
     ]
 
-    def visit_for(self, node: astroid.Call):
+    def visit_module(self, module: astroid.For):
+        """Visit module and infer which library the variables are from. """
+        try:
+            self._variable_types = TypeInference.infer_variable_types(module)
+        except: # pylint: disable = bare-except
+            ExceptionHandler.handle(self, module)
+
+    def visit_for(self, node: astroid.For):
         """Evaluate whether memory is freed in a loop with model creation."""
         has_clear_session = False
+        has_model_creation = False
 
-        for item in node.body:
-            # if there is no clear_session call before calling a model, the rule is violated.
-            if item.value.func.attrname == "clear_session":
+        for n in node.body:
+            if (
+                hasattr(n, "value")
+                and hasattr(n.value, "func")
+                and hasattr(n.value.func, "attrname")
+                and n.value.func.attrname == "clear_session"
+            ):
                 has_clear_session = True
-            if (item.value.func.attrname in self.MODELS and has_clear_session is False):
-                self.add_message("memory-release-tensorflow", node = node)
+
+            if (
+                hasattr(n, "value")
+                and hasattr(n.value, "func")
+                and hasattr(n.value.func, "attrname")
+                and n.value.func.attrname in self.MODELS
+            ):
+                if(
+                    hasattr(n, "targets")
+                    and len(n.targets) > 0
+                    and hasattr(n.targets[0], "name")
+                    and self._is_tf_variable(n.targets[0].name)
+                ):
+                    has_model_creation = True
+
+        if(
+            has_clear_session == False
+            and has_model_creation == True
+        ):
+            # if there is no clear_session call in the loop while there is a model creation, the rule is violated.
+            self.add_message("memory-release-tensorflow", node = node)
+
+    def _is_tf_variable(self, name) -> bool:
+        """
+        Check whether the variable with the name is a tensorflow variable
+        :param name: name of the variable
+        :return: True when meeting the requirements
+        """
+        return self._variable_types[name] in ["tf", "tensorflow"]
--- dslinter/test/checkers/test_memory_release_tensorflow.py
+++ dslinter/test/checkers/test_memory_release_tensorflow.py
@@ -10,14 +10,27 @@ class TestMemoryReleaseTensorflowChecker(pylint.testutils.CheckerTestCase):
 
     def test_iteration(self):
         """Check whether a message is added is there is no clear_session() before model creation in the loop"""
-        module_tree = astroid.parse("import tensorflow as tf\nfor _ in range(100):\n\tmodel = tf.keras.Sequential([tf.keras.layers.Dense(10) for _ in range(10)])")
-        for_node = module_tree.body[-1]
+        script = """
+            import tensorflow as tf
+            for _ in range(100): #@
+                model = tf.keras.Sequential([tf.keras.layers.Dense(10) for _ in range(10)]) 
+        """
+        module_tree = astroid.parse(script)
+        for_node = astroid.extract_node(script)
         with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id = "memory-release-tensorflow", node = for_node),):
+            self.checker.visit_module(module_tree)
             self.checker.visit_for(for_node)
 
     def test_memory_clear_in_iteration(self):
         """Check whether no message is added is there is a clear_session() before model creation in the loop"""
-        module_tree = astroid.parse("import tensorflow as tf\nfor _ in range(100):\n\ttf.keras.backend.clear_session()\n\tmodel = tf.keras.Sequential([tf.keras.layers.Dense(10) for _ in range(10)])")
-        for_node = module_tree.body[-1]
+        script = """
+            import tensorflow as tf
+            for _ in range(100): #@
+                tf.keras.backend.clear_session()
+                model = tf.keras.Sequential([tf.keras.layers.Dense(10) for _ in range(10)])
+        """
+        module_tree = astroid.parse(script)
+        for_node = astroid.extract_node(script)
         with self.assertNoMessages():
+            self.checker.visit_module(module_tree)
             self.checker.visit_for(for_node)
