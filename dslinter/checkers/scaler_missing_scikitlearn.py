54b7aa38469266d7ad494bf8d748664129bd01ce
Hynn01 <hynn0633@163.com>
2022-02-11 13:16:29 +0100

    change pca to scling-sensitive operation

--- dslinter/checkers/scaler_missing_scikitlearn.py
+++ dslinter/checkers/scaler_missing_scikitlearn.py
@@ -13,13 +13,13 @@ class ScalerMissingScikitLearnChecker(BaseChecker):
 
     __implements__ = IAstroidChecker
 
-    name = "pca-scaler"
+    name = "scalar_missing_scikitlearn"
     priority = -1
     msgs = {
         "W5508": (
-            "Scaler is not used before PCA",
+            "Scaler is not used before scaling-sensitive operation",
             "scaler-missing-scikitlearn",
-            "To ensure a good result, use feature scaling before Principle Component Analysis."
+            "To ensure a good result, use feature scaling before scaling-sensitive operation."
         ),
     }
     options = ()
@@ -29,7 +29,7 @@ class ScalerMissingScikitLearnChecker(BaseChecker):
         "Pipeline",
     ]
 
-    PCA = ["PCA", "KernelPCA", "SparsePCA", "IncrementalPCA", "SVC"]
+    SCALING_SENSITIVE_OPERATIONS = ["PCA", "KernelPCA", "SparsePCA", "IncrementalPCA", "SVC"]
 
     SCALER = ["RobustScaler", "StandardScaler", "MaxAbsScaler", "MinMaxScaler",]
 
@@ -49,45 +49,46 @@ class ScalerMissingScikitLearnChecker(BaseChecker):
         :return:
         """
         try:
-            # If there is no scaler before a pca, the rule is violated.
+            # If there is no scaler before a scaling-sensitive operarion, the rule is violated.
+            # if pipeline is used
             if (
                 hasattr(node, "func")
-                and node.func is not None
                 and hasattr(node.func, "name")
                 and node.func.name in self.PIPELINE
-                and node.args is not None
+                and hasattr(node, "args")
             ):
-                has_pca = False
+                has_scaling_sensitive_operation = False
                 has_scaler = False
                 for arg in node.args:
                     if isinstance(arg, astroid.Call):
-                        if ScalerMissingScikitLearnChecker._call_initiates_scaler(arg):
+                        if self._call_initiates_scaler(arg):
                             has_scaler = True
-                        if ScalerMissingScikitLearnChecker._call_initiates_pca(arg):
-                            has_pca = True
+                        if self._call_initiates_scaling_sensitive_operations(arg):
+                            has_scaling_sensitive_operation = True
                             break
                     if isinstance(arg, astroid.node_classes.List):
-                        for kid in arg.get_children():
-                            for item in kid.get_children():
+                        for ch in arg.get_children():
+                            for item in ch.get_children():
                                 if isinstance(item, astroid.Call):
-                                    if ScalerMissingScikitLearnChecker._call_initiates_scaler(item):
+                                    if self._call_initiates_scaler(item):
                                         has_scaler=True
-                                    if ScalerMissingScikitLearnChecker._call_initiates_pca(item):
-                                        has_pca=True
-                            if has_pca is True:
+                                    if self._call_initiates_scaling_sensitive_operations(item):
+                                        has_scaling_sensitive_operation=True
+                            if has_scaling_sensitive_operation is True:
                                 break
-                if has_pca is True and has_scaler is False:
-                    self.add_message("pca scaler checker", node=node)
+                if has_scaling_sensitive_operation is True and has_scaler is False:
+                    self.add_message("scaler-missing-scikitlearn", node=node)
 
+            # if pipeline is not used
             if (
                     hasattr(node, "func")
-                    and node.func is not None
                     and hasattr(node.func, "attrname")
                     and node.func.attrname in self.LEARNING_FUNCTIONS
-                    and self._expr_is_pca(node.func.expr)
-                    and node.args is not None
+                    and hasattr(node.func, "expr")
+                    and self._expr_is_scaling_sensitive_operation(node.func.expr)
+                    and hasattr(node, "args")
             ):
-                has_pca = True
+                has_scaling_sensitive_operation = True
                 has_scaler = False
                 for arg in node.args:
                     if isinstance(arg, astroid.Name):
@@ -108,16 +109,14 @@ class ScalerMissingScikitLearnChecker(BaseChecker):
                                     and self._expr_is_scaler(value.func.expr.func.expr)
                                 ):
                                     has_scaler = True
-                if has_pca is True and has_scaler is False:
-                    self.add_message("pca scaler checker", node=node)
-
+                if has_scaling_sensitive_operation is True and has_scaler is False:
+                    self.add_message("scaler-missing-scikitlearn", node=node)
 
         except:  # pylint: disable=bare-except
             ExceptionHandler.handle(self, node)
             traceback.print_exc()
 
-    @staticmethod
-    def _call_initiates_pca(call: astroid.Call) -> bool:
+    def _call_initiates_scaling_sensitive_operations(self, call: astroid.Call) -> bool:
         """
         Evaluate whether a Call node is initiating an pca.
 
@@ -125,13 +124,12 @@ class ScalerMissingScikitLearnChecker(BaseChecker):
         :return: True when an estimator is initiated.
         """
         return (
-                call.func is not None
+                hasattr(call, "func")
                 and hasattr(call.func, "name")
-                and call.func.name in ScalerMissingScikitLearnChecker.PCA
+                and call.func.name in self.SCALING_SENSITIVE_OPERATIONS
         )
 
-    @staticmethod
-    def _call_initiates_scaler(call: astroid.Call) -> bool:
+    def _call_initiates_scaler(self, call: astroid.Call) -> bool:
         """
         Evaluate whether a Call node is initiating an scaler.
 
@@ -139,45 +137,43 @@ class ScalerMissingScikitLearnChecker(BaseChecker):
         :return: True when an estimator is initiated.
         """
         return (
-                call.func is not None
+                hasattr(call, "func")
                 and hasattr(call.func, "name")
-                and call.func.name in ScalerMissingScikitLearnChecker.SCALER
+                and call.func.name in self.SCALER
         )
 
-    @staticmethod
-    def _expr_is_pca(expr: astroid.node_classes.NodeNG) -> bool:
+    def _expr_is_scaling_sensitive_operation(self, expr: astroid.node_classes.NodeNG) -> bool:
         """
         Evaluate whether the expression is an estimator.
 
         :param expr: Expression to evaluate.
         :return: True when the expression is an estimator.
         """
-        if isinstance(expr, astroid.Call) and ScalerMissingScikitLearnChecker._call_initiates_pca(expr):
+        if isinstance(expr, astroid.Call) and self._call_initiates_scaling_sensitive_operations(expr):
             return True
 
         # If expr is a Name, check whether that name is assigned to an estimator.
         if isinstance(expr, astroid.Name):
             values = AssignUtil.assignment_values(expr)
             for value in values:
-                if ScalerMissingScikitLearnChecker._expr_is_pca(value):
+                if self._expr_is_scaling_sensitive_operation(value):
                     return True
         return False
 
-    @staticmethod
-    def _expr_is_scaler(expr: astroid.node_classes.NodeNG) -> bool:
+    def _expr_is_scaler(self, expr: astroid.node_classes.NodeNG) -> bool:
         """
         Evaluate whether the expression is an estimator.
 
         :param expr: Expression to evaluate.
         :return: True when the expression is an estimator.
         """
-        if isinstance(expr, astroid.Call) and ScalerMissingScikitLearnChecker._call_initiates_scaler(expr):
+        if isinstance(expr, astroid.Call) and self._call_initiates_scaler(expr):
             return True
 
         # If expr is a Name, check whether that name is assigned to an estimator.
         if isinstance(expr, astroid.Name):
             values = AssignUtil.assignment_values(expr)
             for value in values:
-                if ScalerMissingScikitLearnChecker._expr_is_scaler(value):
+                if self._expr_is_scaler(value):
                     return True
         return False
--- dslinter/test/checkers/test_scaler_missing_scikitlearn.py
+++ dslinter/test/checkers/test_scaler_missing_scikitlearn.py
@@ -8,34 +8,83 @@ class TestScalerMissingScikitLearn(pylint.testutils.CheckerTestCase):
 
     CHECKER_CLASS = dslinter.plugin.ScalerMissingScikitLearnChecker
 
-    # def test_PCA_missing_scaling(self):
-    #     node = astroid.extract_node("""
-    #         from sklearn.datasets import load_wine
-    #         from sklearn.model_selection import train_test_split
-    #         from sklearn.pipeline import make_pipeline
-    #         from sklearn.decomposition import PCA
-    #         from sklearn.naive_bayes import GaussianNB
-    #         from sklearn.metrics import accuracy_score
-    #
-    #         RANDOM_STATE = 42
-    #         features, target = load_wine(return_X_y=True)
-    #         X_train, X_test, y_train, y_test = train_test_split(
-    #             features, target, test_size=0.30, random_state=RANDOM_STATE
-    #         )
-    #         clf = make_pipeline(PCA(n_components=2), GaussianNB()) #@
-    #         clf.fit(X_train, y_train)
-    #         pred_test = clf.predict(X_test)
-    #         ac = accuracy_score(y_test, pred_test)
-    #     """)
-    #     with self.assertAddsMessages(
-    #         pylint.testutils.MessageTest(
-    #             msg_id= 'scaler-missing-scikitlearn',
-    #             node = node,
-    #         )
-    #     ):
-    #         self.checker.visit_call(node)
+    def test_PCA_missing_scaling(self):
+        assign_node = astroid.extract_node("""
+            from sklearn.datasets import load_wine
+            from sklearn.model_selection import train_test_split
+            from sklearn.pipeline import make_pipeline
+            from sklearn.decomposition import PCA
+            from sklearn.naive_bayes import GaussianNB
+            from sklearn.metrics import accuracy_score
+
+            RANDOM_STATE = 42
+            features, target = load_wine(return_X_y=True)
+            X_train, X_test, y_train, y_test = train_test_split(
+                features, target, test_size=0.30, random_state=RANDOM_STATE
+            )
+            clf = make_pipeline(PCA(n_components=2), GaussianNB()) #@
+            clf.fit(X_train, y_train)
+            pred_test = clf.predict(X_test)
+            ac = accuracy_score(y_test, pred_test)
+        """)
+        call_node = assign_node.value
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(
+                msg_id= 'scaler-missing-scikitlearn',
+                node = call_node,
+            )
+        ):
+            self.checker.visit_call(call_node)
 
     def test_PCA_with_scaling(self):
+        assign_node = astroid.extract_node("""
+            from sklearn.datasets import load_wine
+            from sklearn.model_selection import train_test_split
+            from sklearn.pipeline import make_pipeline
+            from sklearn.decomposition import PCA
+            from sklearn.naive_bayes import GaussianNB
+            from sklearn.metrics import accuracy_score
+            from sklearn.preprocessing import StandardScaler
+
+            RANDOM_STATE = 42
+            features, target = load_wine(return_X_y=True)
+            X_train, X_test, y_train, y_test = train_test_split(
+                features, target, test_size=0.30, random_state=RANDOM_STATE
+            )
+            clf = make_pipeline(StandardScaler(), PCA(n_components=2), GaussianNB()) #@
+            clf.fit(X_train, y_train)
+            pred_test = clf.predict(X_test)
+            ac = accuracy_score(y_test, pred_test)
+        """)
+        call_node = assign_node.value
+        with self.assertNoMessages():
+            self.checker.visit_call(call_node)
+
+    def test_SVC_missing_scaling(self):
+        node = astroid.extract_node("""
+            from sklearn.datasets import load_wine
+            from sklearn.model_selection import train_test_split
+            from sklearn.pipeline import make_pipeline
+            from sklearn.decomposition import PCA
+            from sklearn.naive_bayes import GaussianNB
+            from sklearn.metrics import accuracy_score
+            from sklearn.preprocessing import StandardScaler
+
+            RANDOM_STATE = 42
+            features, target = load_wine(return_X_y=True)
+            X_train, X_test, y_train, y_test = train_test_split(
+                features, target, test_size=0.30, random_state=RANDOM_STATE
+            )
+            clf = make_pipeline(StandardScaler(), PCA(n_components=2), GaussianNB()) #@
+            clf.fit(X_train, y_train)
+            pred_test = clf.predict(X_test)
+            ac = accuracy_score(y_test, pred_test)
+        """)
+        with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id='scaler-missing-scikitlearn', node = node)):
+            self.checker.visit_call(node)
+
+
+    def test_SVC_with_scaling(self):
         node = astroid.extract_node("""
             from sklearn.datasets import load_wine
             from sklearn.model_selection import train_test_split
@@ -57,50 +106,3 @@ class TestScalerMissingScikitLearn(pylint.testutils.CheckerTestCase):
         """)
         with self.assertNoMessages():
             self.checker.visit_call(node)
-
-    # def test_SVC_missing_scaling(self):
-    #     node = astroid.extract_node("""
-    #         from sklearn.datasets import load_wine
-    #         from sklearn.model_selection import train_test_split
-    #         from sklearn.pipeline import make_pipeline
-    #         from sklearn.decomposition import PCA
-    #         from sklearn.naive_bayes import GaussianNB
-    #         from sklearn.metrics import accuracy_score
-    #         from sklearn.preprocessing import StandardScaler
-    #
-    #         RANDOM_STATE = 42
-    #         features, target = load_wine(return_X_y=True)
-    #         X_train, X_test, y_train, y_test = train_test_split(
-    #             features, target, test_size=0.30, random_state=RANDOM_STATE
-    #         )
-    #         clf = make_pipeline(StandardScaler(), PCA(n_components=2), GaussianNB()) #@
-    #         clf.fit(X_train, y_train)
-    #         pred_test = clf.predict(X_test)
-    #         ac = accuracy_score(y_test, pred_test)
-    #     """)
-    #     with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id='scaler-missing-scikitlearn', node = node)):
-    #         self.checker.visit_call(node)
-    #
-    #
-    # def test_SVC_with_scaling(self):
-    #     node = astroid.extract_node("""
-    #         from sklearn.datasets import load_wine
-    #         from sklearn.model_selection import train_test_split
-    #         from sklearn.pipeline import make_pipeline
-    #         from sklearn.decomposition import PCA
-    #         from sklearn.naive_bayes import GaussianNB
-    #         from sklearn.metrics import accuracy_score
-    #         from sklearn.preprocessing import StandardScaler
-    #
-    #         RANDOM_STATE = 42
-    #         features, target = load_wine(return_X_y=True)
-    #         X_train, X_test, y_train, y_test = train_test_split(
-    #             features, target, test_size=0.30, random_state=RANDOM_STATE
-    #         )
-    #         clf = make_pipeline(StandardScaler(), PCA(n_components=2), GaussianNB()) #@
-    #         clf.fit(X_train, y_train)
-    #         pred_test = clf.predict(X_test)
-    #         ac = accuracy_score(y_test, pred_test)
-    #     """)
-    #     with self.assertNoMessages():
-    #         self.checker.visit_call(node)
