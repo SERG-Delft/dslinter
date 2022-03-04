adf226c57a4187b08ae03386bc451561b1a9792d
Hynn01 <hynn0633@163.com>
2022-03-04 14:17:20 +0100

    inspect randomness at module level - tensorflow

--- dslinter/checkers/randomness_controlling_tensorflow.py
+++ dslinter/checkers/randomness_controlling_tensorflow.py
@@ -31,25 +31,28 @@ class RandomnessControllingTensorflowChecker(BaseChecker):
             if name == "tensorflow":
                 self._import_tensorflow = True
 
-    def visit_call(self, node: astroid.Call):
+    def visit_module(self, module: astroid.Module):
         """
         Check whether there is a rule violation.
         :param node:
         """
-        if(
-            hasattr(node, "func")
-            and hasattr(node.func, "attrname")
-            and node.func.attrname == "set_seed"
-            and hasattr(node.func.expr, "attrname")
-            and node.func.expr.attrname == "random"
-            and hasattr(node.func.expr, "expr")
-            and hasattr(node.func.expr.expr, "name")
-            and node.func.expr.expr.name in ["tf", "tensorflow"]
-        ):
-            self._has_manual_seed = True
+        for node in module.body:
+            if isinstance(node, astroid.nodes.Expr) and hasattr(node, "value"):
+                call_node = node.value
+                if(
+                    hasattr(call_node, "func")
+                    and hasattr(call_node.func, "attrname")
+                    and call_node.func.attrname == "set_seed"
+                    and hasattr(call_node.func.expr, "attrname")
+                    and call_node.func.expr.attrname == "random"
+                    and hasattr(call_node.func.expr, "expr")
+                    and hasattr(call_node.func.expr.expr, "name")
+                    and call_node.func.expr.expr.name in ["tf", "tensorflow"]
+                ):
+                    self._has_manual_seed = True
 
         if(
             self._import_tensorflow is True
             and self._has_manual_seed is False
         ):
-            self.add_message("randomness-control-tensorflow", node = node)
+            self.add_message("randomness-control-tensorflow", node = module)
