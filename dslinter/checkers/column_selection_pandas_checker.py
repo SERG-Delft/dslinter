658c59b4183db72f59351e1880523181eb209f3c
Hynn01 <hynn0633@163.com>
2022-03-18 14:12:31 +0100

    add ColumnSelectionPandasChecker

--- dslinter/checkers/column_selection_pandas_checker.py
+++ dslinter/checkers/column_selection_pandas_checker.py
@@ -1,3 +1,4 @@
+import astroid as astroid
 from pylint.checkers import BaseChecker
 from pylint.interfaces import IAstroidChecker
 
@@ -16,6 +17,37 @@ class ColumnSelectionPandasChecker(BaseChecker):
     }
     options = ()
 
-    def visit_call(self):
-        pass
+    _import_data_pandas = False
+    _import_data_idx = -1
+    _dataframe_name = ""
+
+    def visit_module(self, module: astroid.Module):
+        for idx, node in enumerate(module.body):
+            if isinstance(node, astroid.Assign):
+                sub_node = node.value
+                if(
+                    hasattr(sub_node, "func")
+                    and hasattr(sub_node.func, "attrname")
+                    and sub_node.func.attrname == "read_csv"
+                    and hasattr(sub_node.func, "expr")
+                    and hasattr(sub_node.func.expr, "name")
+                    and sub_node.func.expr.name in ["pandas", "pd"]
+                    and len(node.targets) > 0
+                    and hasattr(node.targets[0], "name")
+                ):
+                    self._import_data_pandas = True
+                    self._import_data_idx = idx
+                    self._dataframe_name = node.targets[0].name
+                if self._import_data_idx != -1 and idx == self._import_data_idx + 1:
+                    if(
+                        len(node.targets) > 0
+                        and hasattr(node.targets[0], "name")
+                        and node.targets[0].name == self._dataframe_name
+                        and hasattr(node, "value")
+                        and hasattr(node.value, "slice")
+                        and isinstance(node.value.slice, astroid.nodes.List)
+                    ):
+                        pass
+                    else:
+                        self.add_message("column-selection-pandas", node=module)
 
--- dslinter/tests/checkers/test_column_selection_pandas_checker.py
+++ dslinter/tests/checkers/test_column_selection_pandas_checker.py
@@ -1,26 +1,28 @@
-# import pylint.testutils
-#
-# import dslinter.plugin
-#
-#
-# class TestColumnSelectionPandasChecker(pylint.testutils.CheckerTestCase):
-#
-#     CHECKER_CLASS = dslinter.plugin.ColumnSelectionPandasChecker
-#
-#     def test_column_not_selected(self):
-#         script = """
-#         import pandas as pd
-#         df = pd.read_csv('data.csv')
-#         """
-#         with self.assertAddsMessages():
-#             self.checker.visit_call()
-#
-#
-#     def test_column_selected(self):
-#         script = """
-#         import pandas as pd
-#         df = pd.read_csv('data.csv')
-#         df = df[['col1', 'col2', 'col3']]
-#         """
-#         with self.assertNoMessages():
-#             self.checker.visit_call()
+import pylint.testutils
+import astroid
+import dslinter.plugin
+
+
+class TestColumnSelectionPandasChecker(pylint.testutils.CheckerTestCase):
+
+    CHECKER_CLASS = dslinter.plugin.ColumnSelectionPandasChecker
+
+    def test_column_not_selected(self):
+        script = """
+        import pandas as pd
+        df = pd.read_csv('data.csv')
+        """
+        module = astroid.parse(script)
+        with self.assertAddsMessages():
+            self.checker.visit_module(module)
+
+
+    def test_column_selected(self):
+        script = """
+        import pandas as pd
+        df = pd.read_csv('data.csv')
+        df = df[['col1', 'col2', 'col3']]
+        """
+        module = astroid.parse(script)
+        with self.assertNoMessages():
+            self.checker.visit_module(module)
