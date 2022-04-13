60372cb11753a81a7815712db5725577f199f2b2
Hynn01 <hynn0633@163.com>
2022-04-13 16:28:27 +0200

    refactor TestImportChecker

--- dslinter/tests/checkers/test_imports.py
+++ dslinter/tests/checkers/test_imports.py
@@ -10,132 +10,108 @@ class TestImportChecker(pylint.testutils.CheckerTestCase):
 
     def test_finds_incorrect_pandas(self):
         """Test if message is added when pandas is imported incorrectly."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             import pandas as pand #@
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id="import-pandas", node=import_node),):
             self.checker.visit_import(import_node)
 
     def test_ignore_correct_pandas(self):
         """Test if no message is added when pandas is imported correctly."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             import pandas as pd #@
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertNoMessages():
             self.checker.visit_import(import_node)
 
     def test_finds_incorrect_numpy(self):
         """Test if message is added when numpy is imported incorrectly."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             import numpy #@
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id="import-numpy", node=import_node),):
             self.checker.visit_import(import_node)
 
     def test_ignore_correct_numpy(self):
         """Test if no message is added when numpy is imported correctly."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             import numpy as np #@
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertNoMessages():
             self.checker.visit_import(import_node)
 
     def test_finds_incorrect_pyplot(self):
         """Test if message is added when pyplot is imported incorrectly."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             import matplotlib.pyplot as plot #@
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id="import-pyplot", node=import_node),):
             self.checker.visit_import(import_node)
 
     def test_ignore_correct_pyplot(self):
         """Test if no message is added when pyplot is imported correctly."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             import matplotlib.pyplot as plt #@
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertNoMessages():
             self.checker.visit_import(import_node)
 
     def test_finds_incorrect_sklearn(self):
         """Test if message is added when an import from the sklearn module is assigned an alias."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             from sklearn.cluster import KMeans as km #@
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id="import-sklearn", node=import_node),):
             self.checker.visit_import_from(import_node)
 
     def test_ignore_correct_sklearn(self):
         """Test if no message is added when an sklearn module import is not assigned an alias."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             from sklearn.cluster import KMeans #@
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertNoMessages():
             self.checker.visit_import_from(import_node)
 
     def test_finds_incorrect_tensorflow(self):
         """Test if message is added when tensorflow is imported incorrectly."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             import tensorflow as tff
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id="import-tensorflow", node=import_node)):
             self.checker.visit_import(import_node)
 
     def test_ignore_correct_tensorflow(self):
         """Test if no message is added when tensorflow is imported correctly."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             import tensorflow as tf
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertNoMessages():
             self.checker.visit_import(import_node)
 
     def test_finds_incorrect_pytorch(self):
         """Test if message is added when pytorch is imported incorrectly."""
-        import_node = astroid.extract_node(
-            """
+        script = """
             import torch as tr
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id="import-pytorch", node=import_node)):
             self.checker.visit_import(import_node)
 
     def test_ignore_correct_pytorch(self):
         """Test if no message is added when pytorch is imported correclty"""
-        import_node = astroid.extract_node(
-            """
+        script =  """
             import torch
             """
-        )
-
+        import_node = astroid.extract_node(script)
         with self.assertNoMessages():
             self.checker.visit_import(import_node)
