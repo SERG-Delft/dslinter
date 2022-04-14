854f820b524e6ccb6ee4ff2ee27add8c4f1ea126
Hynn01 <hynn0633@163.com>
2022-04-14 16:36:18 +0200

    update deterministic-pytorch checker

--- dslinter/checkers/randomness_control_dataloader_pytorch.py
+++ dslinter/checkers/randomness_control_dataloader_pytorch.py
@@ -19,22 +19,57 @@ class RandomnessControlDataloaderPytorchChecker(BaseChecker):
     }
     options = ()
 
+    _import_DataLoader = False
+
+    def visit_import_from(self, import_from_node: astroid.ImportFrom):
+        """
+        Check whether there is DataLoader imported.
+        """
+        if(
+            hasattr(import_from_node, "modname")
+            and import_from_node.modname == "torch.utils.data"
+            and hasattr(import_from_node, "names")
+        ):
+            for name, _ in import_from_node.names:
+                if name == "DataLoader":
+                    self._import_DataLoader = True
+
     def visit_call(self, node: astroid.Call):
         """
         Check whether there is a rule violation.
         :param node:
         """
-        keywords = []
-        if(
-            hasattr(node.func, "name")
-            and node.func.name == "DataLoader"
-        ):
+        if self._use_dataloader_from_import(node) or self._use_dataloader_from_torch(node):
+            # In dataloader, check if "worker_init_fn" and "generator" is set.
+            keywords = []
             if hasattr(node, "keywords"):
                 for k in node.keywords:
                     if hasattr(k, "arg"):
                         keywords.append(k.arg)
-            if(
-                "worker_init_fn" not in keywords
-                or "generator" not in keywords
-            ):
+            if "worker_init_fn" not in keywords or "generator" not in keywords:
                 self.add_message("randomness-control-dataloader-pytorch", node=node)
+
+    def _use_dataloader_from_import(self, node):
+        # Dataloader has been imported from torch.utils.data
+        if(
+            self._import_DataLoader is True
+            and hasattr(node.func, "name")
+            and node.func.name == "DataLoader"
+        ):
+            return True
+        return False
+
+    def _use_dataloader_from_torch(self, node):
+        # Dataloader has not been imported from torch.utils.data
+        full_expr = ""
+        if hasattr(node, "func"):
+            node = node.func
+            while hasattr(node, "expr"):
+                if hasattr(node, "attrname"):
+                    full_expr = node.attrname + "." + full_expr
+                node = node.expr
+            if hasattr(node, "name"):
+                full_expr = node.name + "." + full_expr
+            if full_expr[:-1] == "torch.utils.data.DataLoader":
+                return True
+        return False
--- dslinter/tests/checkers/test_randomness_control_dataloader_pytorch.py
+++ dslinter/tests/checkers/test_randomness_control_dataloader_pytorch.py
@@ -9,12 +9,13 @@ class TestRandomnessControlDataloaderPytorchChecker(pylint.testutils.CheckerTest
 
     CHECKER_CLASS = dslinter.plugin.RandomnessControlDataloaderPytorchChecker
 
-    def test_with_dataloader_randomness_control(self):
+    def test_with_dataloader_randomness_control1(self):
         """Tests whether no message is added if manual seed is set."""
         script = """
             import torch
             import numpy
             import random
+            from torch.utils.data import DataLoader #@
 
             def seed_worker(worker_id):
                 worker_seed = torch.initial_seed() % 2**32
@@ -36,20 +37,69 @@ class TestRandomnessControlDataloaderPytorchChecker(pylint.testutils.CheckerTest
                 generator=g,
             )
         """
-        call_node = astroid.extract_node(script).value
+        import_from_node, assign_node = astroid.extract_node(script)
+        call_node = assign_node.value
         with self.assertNoMessages():
+            self.checker.visit_import_from(import_from_node)
             self.checker.visit_call(call_node)
 
-    def test_without_dataloader_randomness_control(self):
+    def test_without_dataloader_randomness_control1(self):
         """Tests whether a message is added if manual seed is not set"""
         script = """
-            from torch.utils.data import DataLoader
+            from torch.utils.data import DataLoader #@
             train_dataloader = DataLoader( #@
                 train_dataset,
                 batch_size=batch_size,
                 num_workers=num_workers
             )
         """
+        import_from_node, assign_node = astroid.extract_node(script)
+        call_node = assign_node.value
+        with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id="randomness-control-dataloader-pytorch", node=call_node)):
+            self.checker.visit_import_from(import_from_node)
+            self.checker.visit_call(call_node)
+
+    def test_with_dataloader_randomness_control2(self):
+        """Tests whether no message is added if manual seed is set."""
+        script = """
+            import torch
+            import numpy
+            import random
+
+            def seed_worker(worker_id):
+                worker_seed = torch.initial_seed() % 2**32
+                numpy.random.seed(worker_seed)
+                random.seed(worker_seed)
+
+            g = torch.Generator()
+            g.manual_seed(0)
+
+            batch_size = 4
+            num_workers = 4
+            train_dataset = [1, 1, 1, 1]
+
+            train_dataloader = torch.utils.data.DataLoader( #@
+                train_dataset,
+                batch_size=batch_size,
+                num_workers=num_workers,
+                worker_init_fn=seed_worker,
+                generator=g,
+            )
+        """
         call_node = astroid.extract_node(script).value
-        with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id="randomness-control-dataloader-pytorch", node = call_node)):
+        with self.assertNoMessages():
+            self.checker.visit_call(call_node)
+
+    def test_without_dataloader_randomness_control2(self):
+        """Tests whether a message is added if manual seed is not set"""
+        script = """
+            from torch.utils.data import DataLoader
+            train_dataloader = torch.utils.data.DataLoader( #@
+                train_dataset,
+                batch_size=batch_size,
+                num_workers=num_workers
+            )
+        """
+        call_node = astroid.extract_node(script).value
+        with self.assertAddsMessages(pylint.testutils.MessageTest(msg_id="randomness-control-dataloader-pytorch", node=call_node)):
             self.checker.visit_call(call_node)
