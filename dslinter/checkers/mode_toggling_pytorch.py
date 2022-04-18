7fd810e3038bc1ae2f2da8f6ce68a94706985a9f
Hynn01 <hynn0633@163.com>
2022-04-18 14:14:25 +0200

    update mode-toggling-pytorch checker

--- dslinter/checkers/mode_toggling_pytorch.py
+++ dslinter/checkers/mode_toggling_pytorch.py
@@ -29,27 +29,22 @@ class ModeTogglingPytorchChecker(BaseChecker):
         _has_eval = False
         for node in for_node.body:
             if isinstance(node, astroid.Expr):
-                if(
-                    isinstance(node.value, astroid.Call)
-                    and hasattr(node.value.func, "attrname")
-                    and node.value.func.attrname == "train"
-                ):
-                    _has_train = True
-                if(
-                    isinstance(node.value, astroid.Call)
-                    and hasattr(node.value.func, "attrname")
-                    and node.value.func.attrname == "eval"
-                ):
-                    _has_eval = True
+                if isinstance(node.value, astroid.Call) and hasattr(node.value.func, "attrname"):
+                    if node.value.func.attrname == "train":
+                        _has_train = True
+                    if node.value.func.attrname == "eval":
+                        _has_eval = True
             if isinstance(node, astroid.If):
                 for if_node in node.body:
                     if(
                         isinstance(if_node, astroid.Expr)
                         and isinstance(if_node.value, astroid.Call)
                         and hasattr(if_node.value.func, "attrname")
-                        and if_node.value.func.attrname == "eval"
                     ):
-                        _has_eval = True
+                        if if_node.value.func.attrname == "train":
+                            _has_train = True
+                        if if_node.value.func.attrname == "eval":
+                            _has_eval = True
 
-        if _has_eval != _has_train:
+        if _has_eval is True and _has_train is False:
             self.add_message("mode-toggling-pytorch", node=for_node)
